96808b5b66cf98692e3120f9dabf9e295d45cd1c
kido <61651321+syskido@users.noreply.github.com>
2025-12-15 18:54:09 +0900

    ttt

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{   
+    public float maxSpeed;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed ) //Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if (rigid.velocity.x < maxSpeed*(-1)) //Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+
+
+    }
+}
